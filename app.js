3727b9c5e932f2550432221c1199f45db4ae13ae
gitvinit <vinitbadrike@yahoo.co.in>
2018-10-24 14:36:14 -0500

    Added auth

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 var userRouter = require('./user/user-routes')();
 
 app.use('/users', userRouter);
+var AuthController = require('./auth/auth-controller');
+app.use('/api/auth', AuthController);
 app.get('/', function (req, res) {
     res.send("Hello");
 });
